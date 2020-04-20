32f2674d2858726d0324aeca4bd360d6d794c3ae
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2020-04-20 13:09:15 +0400

    Add hist visual, weight norm tracker, customizability
    **Features**:
     - Added `features_hist_v2` for multi-hist grid visualization
     - Added `weights_norm` for computing & tracking layer weight norm statistics
     - Enabled full customization of various plot aspects via `configs` dict, passed as kwargs; see docstrings and source code
    
    **Breaking**:
      - Several positional & keyword arguments deprecated in favor of `configs`

--- see_rnn/inspect_gen.py
+++ see_rnn/inspect_gen.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 
 from termcolor import colored
+from copy import deepcopy
 from .utils import _validate_args
 
 
@@ -72,9 +73,10 @@ def get_layer_gradients(model, input_data, labels, layer_name=None,
 
     (1): tf.data.Dataset, generators, .tfrecords, & other supported TensorFlow
          input data formats
-    (2): "Outputs" are not necessarily activations. If an Activation layer is used,
-         returns the gradients of the target layer's PRE-activations instead. To
-         then get grads w.r.t. activations, specify the Activation layer.
+    (2): "Outputs" are not necessarily activations. If an Activation layer is
+         used, returns the gradients of the target layer's PRE-activations
+         instead. To then get grads w.r.t. activations, specify the
+         Activation layer.
     """
 
     def _validate_args_(layer_name, layer_idx, layer, mode):
@@ -144,3 +146,111 @@ def _detect_nans(data):
     else:
         txt = "%.1f" % perc_nans + "% \nNaNs"  # show as percent
     return txt
+
+
+def weights_norm(model, names, _dict=None, stat_fns=(np.max, np.mean),
+                 norm_fn=np.square, omit_weight_names=None, axis=-1, verbose=0):
+    """Retrieves model layer weight matrix norms, as specified by `norm_fn`.
+
+    Arguments:
+        model: keras.Model/tf.keras.Model.
+        names: str list. List of names (can be substring) of layers to fetch
+               weights from.
+        _dict: dict/None. If None, returns new dict. If dict, appends to it.
+        stat_fns: functions list/tuple. Aggregate statistic to compute from
+               normed weights.
+        norm_fn: function. Norm transform to apply to weights. Ex:
+              - np.square (l2 norm)
+              - np.abs    (l1 norm)
+        omit_weight_names: str list. List of names (can be substring) of weights
+               to omit from fetching.
+        axis: int. Axis w.r.t. which compute the norm (collapsing all others).
+        verbose: int/bool, 0/1. 1/True: print norm stats, enumerated by layer
+               indices. If `_dict` is not None, print last computed results.
+
+    Returns:
+        stats_all: dict. dict of lists containing layer weight norm statistics.
+    """
+    def _process_args(model, names, _dict, omit_weight_names):
+        def _process_names(names, model):
+            if isinstance(names, str):
+                names = [names]
+            fullnames = []
+            for layer in model.layers:
+                if any([name in layer.name.lower() for name in names]):
+                    fullnames.append(layer.name)
+            return fullnames
+
+        names = _process_names(names, model)
+
+        if omit_weight_names is None:
+            omit_weight_names = []
+        elif isinstance(omit_weight_names, str):
+            omit_weight_names = [omit_weight_names]
+
+        if _dict:
+            stats_all = deepcopy(_dict)  # do not mutate original dict
+        else:
+            stats_all = {name: [[]] for name in names}
+        return stats_all, names, omit_weight_names
+
+
+    def _print_stats(stats_all, l_idx, l_name):
+        def _unpack_layer_stats(stats_all, l_name):
+            l_stats = stats_all[l_name]
+            stats_flat = []
+            for w_stats in l_stats:
+                if isinstance(w_stats, list):
+                    [stats_flat.extend(s) for s in w_stats]
+                else:
+                    stats_flat.append(w_stats)
+            return stats_flat
+
+        txt = "{} "
+        for w_stats in stats_all[l_name]:
+            txt += "{:.4f}, " * len(w_stats)
+            txt = txt.rstrip(", ") + " -- "
+        txt = txt.rstrip(" -- ")
+
+        stats_flat = _unpack_layer_stats(stats_all, l_name)
+        print(txt.format(l_idx, *stats_flat))
+
+
+    def _get_layer_norm(stats_all, layer, norm_fn, stat_fns, axis=-1):
+        def _compute_norm(w, norm_fn, axis=-1):
+            axis = axis if axis != -1 else len(w.shape) - 1
+            reduction_axes = tuple([ax for ax in range(len(w.shape))
+                                    if ax != axis])
+            return np.sqrt(np.sum(norm_fn(w), axis=reduction_axes))
+
+        def _append(stats_all, l2_stats, w_idx, l_name):
+            if len(stats_all[l_name]) < w_idx + 1:
+                stats_all[l_name].append([])
+            while len(stats_all[l_name][w_idx]) < len(l2_stats):
+                stats_all[l_name][w_idx].append([])
+
+            for stat_idx, stat in enumerate(l2_stats):
+                stats_all[l_name][w_idx][stat_idx].append(stat)
+
+        W = layer.get_weights()
+        w_names = [w.name for w in layer.weights]
+        l_name = layer.name
+
+        for w_idx, (w, w_name) in enumerate(zip(W, w_names)):
+            if any([to_omit in w_name for to_omit in omit_weight_names]):
+                continue
+            l2 = _compute_norm(w, norm_fn, axis)
+            l2_stats = [fn(l2) for fn in stat_fns]
+            _append(stats_all, l2_stats, w_idx, l_name)
+        return stats_all
+
+    stats_all, names, omit_weight_names = _process_args(
+        model, names, _dict, omit_weight_names)
+
+    for l_idx, layer in enumerate(model.layers):
+        if layer.name in names:
+            stats_all = _get_layer_norm(stats_all, layer, norm_fn,
+                                        stat_fns, axis)
+            if verbose:
+                _print_stats(stats_all, l_idx, layer.name)
+    return stats_all
