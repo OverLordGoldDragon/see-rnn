7e12a29d3bfbace910f9a57a1bd51a2e4cb5b8a1
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2020-04-20 13:26:22 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,26 +18,45 @@ def get_requirements(*parts):
 
 def find_version(*file_paths):
     version_file = read_file(*file_paths)
-    version_matched = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    version_matched = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                version_file, re.M)
     if version_matched:
         return version_matched.group(1)
     raise RuntimeError('Unable to find version')
 
 
 setup(
-    name='see-rnn',
+    name="see-rnn",
     version=find_version('see_rnn', '__init__.py'),
-    packages=find_packages(),
-    url='https://github.com/OverLordGoldDragon/see-rnn',
-    license='MIT',
-    author='OverLordGoldDragon',
-    description='RNN weights, gradients, & activations visualization in Keras & TensorFlow ',
+    packages=find_packages(exclude=['tests']),
+    url="https://github.com/OverLordGoldDragon/see-rnn",
+    license="MIT",
+    author="OverLordGoldDragon",
+    author_email="16495490+OverLordGoldDragon@users.noreply.github.com",
+    description=('RNN weights, gradients, & activations visualization '
+                 'in Keras & TensorFlow'),
     long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
+    keywords=(
+        "rnn tensorflow keras visualization deep-learning lstm gru "
+    ),
     install_requires=get_requirements('requirements.txt'),
+    include_package_data=True,
+    zip_safe=True,
+    tests_require=["pytest>=4.0", "pytest-cov"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
