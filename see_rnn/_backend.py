67e13239fd5f2fb0202caad99a026b755be04fad
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2020-11-11 10:46:48 +0400

    Update _backend.py

--- see_rnn/_backend.py
+++ see_rnn/_backend.py
@@ -2,8 +2,12 @@ import os
 from termcolor import colored
 
 
+TF_KERAS = os.environ.get("TF_KERAS", '1') == '1'
+WARN = colored("WARNING:", 'red')
+NOTE = colored("NOTE:", 'blue')
+
 try:
-    if os.environ.get("TF_KERAS", '1') == '1':
+    if TF_KERAS:
         from tensorflow.python.keras import backend as K
         from tensorflow.keras.layers import Layer
         from tensorflow.keras.models import Model
@@ -15,8 +19,3 @@ except:
     print("WARNING: failed to import TensorFlow or Keras; functionality "
           "is restricted to see_rnn.visuals_gen")
     K, Layer, Model = None, None, None
-
-
-TF_KERAS = os.environ.get("TF_KERAS", '1') == '1'
-WARN = colored("WARNING:", 'red')
-NOTE = colored("NOTE:", 'blue')
